ac004ef6b3c25f880e3429ed864a3dac092fffef
Toriqul Islam <96937038+Toriqulislam1@users.noreply.github.com>
2025-02-03 12:08:33 +0600

    vue practise

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Header from './components/Header.vue'
+
+import test from './components/test.vue'
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <Header msg="Header" />
+      <HelloWorld msg="Y" />
+      <test msg2="zzz" msg3="msg3" msg4="zzz" />
+
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,39 @@
+
+
+<script setup>
+defineProps({
+    msg2: {
+    type: String,
+    required: true,
+  },
+  msg3: {
+    type: String,
+    required: true,
+  },
+
+})
+</script>
+
+
+<template>
+    <div>
+        <h1>Hello, Vue! {{ msg2 }} {{ msg3 }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TestComponent',
+    data() {
+        return {
+            message: 'Welcome to your Vue component!'
+        };
+    }
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
